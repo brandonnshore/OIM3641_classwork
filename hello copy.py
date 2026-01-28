d74e34d9b405dfc92a8ad2eb6800a810ef63d5bb
Brandon Shore <brandonnshore@gmail.com>
2026-01-28 10:50:46 -0500

    class files

---
+++ hello copy.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+<<<<<<< HEAD
+
+  def main():
+      print("Hello, World!")
+      print("testing!")
+
+  if __name__ == "__main__":
+      main()
+=======
+"""
+A simple hello world program to demonstrate Git version control
+"""
+
+def main():
+    print("Hello, World!")
+    print("Learning Git is awesome!")
+
+if __name__ == "__main__":
+    main()
+>>>>>>> 8886272 (First commit)
